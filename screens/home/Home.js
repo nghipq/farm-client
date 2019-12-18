883874a99f24fb5e78add5de482c109b898b38f7
nghipq <nghipqce140179@fpt.edu.vn>
2019-12-18 08:57:38 +0700

    fix style name

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -3,14 +3,14 @@ import {View, Text, StyleSheet} from 'react-native'
 
 export default function Home(props) {
     return(
-        <View style={styles.containner}>
+        <View style={styles.container}>
             <Text>Home</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    containner: {
+    container: {
         backgroundColor: "#BBEEB9",
         height: 1000,
     }
