c9f4dbe479e6e1bd340b544b8cbd79663cb9aacb
thanhxuan10 <xuantttce140149@fpt.edu.vn>
2020-01-02 15:28:15 +0700

    add login

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation'
-
+import axios from 'axios'
 import AppNavigator from './Appnavigator'
 
 const AppContainer = createAppContainer(AppNavigator)
+axios.defaults.baseURL='https://51a08114.ngrok.io/'
 
-export default function App() {
+export default function Features(props) {
   return (
       <AppContainer />
   );
